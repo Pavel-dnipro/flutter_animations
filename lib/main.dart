b5cbcc845e566c7340ee8d92c9775f7a8a090857
Pavlo Fesiuk <56679738+Pavel-dnipro@users.noreply.github.com>
2023-04-11 15:10:44 +0300

    added drawer

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_animations/animated_drawer.dart';
+
+void main() {
+  runApp(
+    const App(),
+  );
+}
+
+class App extends StatelessWidget {
+  const App({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(brightness: Brightness.dark),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      themeMode: ThemeMode.dark,
+      debugShowCheckedModeBanner: false,
+      debugShowMaterialGrid: false,
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedDrawer(
+      drawer: Material(
+        child: Container(
+          color: const Color(0xff24283b),
+          child: ListView.builder(
+            padding: const EdgeInsets.only(left: 100, top: 100),
+            itemCount: 20,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text('Item $index'),
+              );
+            },
+          ),
+        ),
+      ),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Drawer'),
+        ),
+        body: Container(
+          color: const Color(0xff414868),
+        ),
+      ),
+    );
+  }
+}
